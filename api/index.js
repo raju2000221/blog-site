269e059e270403d17299c1b088d86c4aa5a9661b
Raju Islam <bdrajuislam246@gmail.com>
2024-04-21 23:33:55 +0600

    server connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config()
+mongoose.connect(process.env.MONGO).then( () =>{
+    console.log('mongodb connected')
+})
 
 const app = express();
 app.listen(3000, () =>{
-    console.log('port running 300')
+    console.log('port running 3000')
 })
