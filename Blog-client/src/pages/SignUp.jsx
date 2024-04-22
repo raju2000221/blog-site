4e387dc5c19c3cea50380f309cc272c3b40af7ac
Raju Islam <bdrajuislam246@gmail.com>
2024-04-22 12:45:13 +0600

    complite signUp page ui

--- Blog-client/src/pages/SignUp.jsx
+++ Blog-client/src/pages/SignUp.jsx
@@ -1,9 +1,54 @@
+import { Button, Label, TextInput } from 'flowbite-react';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
     return (
-        <div>
-            <h1>signup</h1>
+        <div className='min-h-screen mt-20'>
+            <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row">
+                {/* left side */}
+                <div className=" flex-1 my-auto">
+                <Link to="/" className='self-center  font-bold text-4xl'>
+                <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Blog</span>
+                Post
+            </Link>
+            <p className='text-sm my-5 font-semibold'>You can sign up with your email and password <br /> or Google</p>
+                </div>
+                {/* right side */}
+                <div className=" flex-1">
+                    <form className='flex flex-col gap-5'>
+                        <div className="">
+                            <Label value='Your user Name'/>
+                            <TextInput
+                            placeholder='Username'
+                            type='text'
+                            id='username'
+                            />
+                        </div>
+                        <div className="">
+                            <Label value='Your email'/>
+                            <TextInput
+                            placeholder='Email'
+                            type='text'
+                            id='email'
+                            />
+                        </div>
+                        <div className="">
+                            <Label value='Your Password'/>
+                            <TextInput
+                            placeholder='Password'
+                            type='text'
+                            id='password'
+                            />
+                        </div>
+                        <Button gradientDuoTone='purpleToPink' type='submit'> Sign Up</Button>
+                    </form>
+                    <div className=" flex gap-4 mt-3">
+                        <span>Have an account ?</span>
+                        <Link className='text-blue-500' to='/SignIn'>Sign In</Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import UserRoutes from './routes/User.route.js';
+import AuthRoute from './routes/auth.route.js';
 
 dotenv.config()
 mongoose.connect(process.env.MONGO).then( () =>{
@@ -8,6 +10,23 @@ mongoose.connect(process.env.MONGO).then( () =>{
 })
 
 const app = express();
+
+app.use(express.json());
+
 app.listen(3000, () =>{
     console.log('port running 3000')
+})
+
+
+app.use('/api/user', UserRoutes )
+app.use('/api/auth', AuthRoute )
+
+app.use((err, req, res, next) =>{
+    const statusCode = err.statusCode || 500
+    const message = err.message || 'internal server error';
+    res.status(statusCode).json({
+        success : false,
+        statusCode,
+        message
+    })
 })
