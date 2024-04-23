f0d7570d418ecc2a1be33a0fdf2960500e73a187
Raju Islam <bdrajuislam246@gmail.com>
2024-04-23 19:48:12 +0600

    Done with Google Auth

--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -15,8 +15,13 @@ router.post('/userLogin', async (req, res) => {
             const existingUser = await User.findOne({username : usernameEmail});
 
             if(existingUser || existingEmail ){
+             
                 const user = existingEmail || existingUser;
-
+                if(!(user.password)){
+                    return res.status(401).json({
+                        message: "Invalid Password"
+                    })
+                }
                const comparePassword = bcrypt.compareSync(password, user.password);
                if(comparePassword){
                 const { username, email, _id } = user;
@@ -57,5 +62,58 @@ router.post('/userLogin', async (req, res) => {
         });
     }
 });
+
+router.post('/googleLogin', async (req, res) => {
+    try {
+        const User = client.db('blog').collection('blogUser');
+
+        const { email, name, photoUrl } = req.body;
+        const existingEmail = await User.findOne({email : email});
+
+        if(existingEmail){
+            if(existingEmail){
+                const newUser= {
+                    email,
+                    name,
+                    photoUrl,
+                }
+                const token = jwt.sign({id: existingEmail._id}, process.env.JWT_SEC)
+                res.status(200).cookie('access_token', token, {
+                    httpOnly:true,
+                }).json(newUser)
+            }
+        }
+        else{
+            const year = currentDate.getFullYear();
+            const month = currentDate.getMonth() + 1;
+            const day = currentDate.getDate();
+            const hours = currentDate.getHours();
+            const minutes = currentDate.getMinutes();
+            const seconds = currentDate.getSeconds();
+            const localDateString = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+            const newUser= {
+                email,
+                name,
+                photoUrl,
+                createdAt : localDateString
+            }
+            const result = await User.insertOne(newUser);
+            const token = jwt.sign({id: newUser._id}, process.env.JWT_SEC)
+            res.status(200).cookie('access_token', token, {
+                httpOnly:true,
+            }).json(newUser)
+        }
+      
+    } catch (error) {
+        console.error('Error signing up user:', error);
+        res.status(500).json({
+            success: false,
+            message: 'Internal server error'
+        });
+    }
+});
+
+
 
 module.exports = router;
